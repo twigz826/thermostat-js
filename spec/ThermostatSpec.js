caca2d93729b4a1956ae4b56bf09bfba359ac526
twigz826 <thomastwigden@gmail.com>
2021-02-10 14:41:59 -0300

    powerSavingMode tests created and passed

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -33,6 +33,29 @@ describe('Thermostat',function(){
     it('throws an error of minimum temperature reached', function(){
       expect(function(){ thermostat.decreaseTemp(11) }).toThrowError('Minimum temperature reached');
     })
+
+  })
+
+  describe('power saving mode',function(){
+
+    it('is on by default with a max temp of 25 degrees',function(){
+      expect(thermostat.showPowerSavingMode()).toBe(true);
+      expect(thermostat.showMaxTemp()).toBe(25);
+    })
+
+    it('can be turned off',function(){
+      thermostat.powerSavingModeOff();
+      expect(thermostat.showPowerSavingMode()).toBe(false);
+      expect(thermostat.showMaxTemp()).toBe(32);
+    })
+
+    it('can be turned on again',function(){
+      thermostat.powerSavingModeOff();
+      thermostat.powerSavingModeOn();
+      expect(thermostat.showPowerSavingMode()).toBe(true);
+      expect(thermostat.showMaxTemp()).toBe(25);
+    })
+
   })
 
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,9 +2,13 @@ class Thermostat {
 
   constructor(){
     this._temperature = 20
+    this._powerSavingMode = true
+    this._maxTemp = 25
   }
 
   showTemp = () => this._temperature;
+  showPowerSavingMode = () => this._powerSavingMode;
+  showMaxTemp = () => this._maxTemp;
 
   increaseTemp = degrees => this._temperature += degrees;
 
@@ -15,4 +19,17 @@ class Thermostat {
     this._temperature -= degrees;
     }
   }
+
+  powerSavingModeOff = () => {
+    this._powerSavingMode = false;
+    this._maxTemp = 32;
+  }
+
+  powerSavingModeOn = () => {
+    this._powerSavingMode = true;
+    this._maxTemp = 25;
+  }
+
+  // changeMaxTemp = () => this._maxTemp = 32;
+
 }
